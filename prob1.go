173348f582ce5af8070cc03bfbbbd8e9b3f95624
Nabanjan Das <nabanjan@gmail.com>
2020-05-08 14:00:39 -0700

    Init

---
+++ prob1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	move := "R4, R4, L1, R3, L5, R2, R5, R1, L4, R3, L5, R2, L3, L4, L3, R1, R5, R1, L3, L1, R3, L1, R2, R2, L2, R5, L3, L4, R4, R4, R2, L4, L1, R5, L1, L4, R4, L1, R1, L2, R5, L2, L3, R2, R1, L194, R2, L4, R49, R1, R3, L5, L4, L1, R4, R2, R1, L5, R3, L5, L4, R4, R4, L2, L3, R78, L5, R4, R191, R4, R3, R1, L2, R1, R3, L1, R3, R4, R2, L2, R1, R4, L5, R2, L2, L4, L2, R1, R2, L3, R5, R2, L3, L3, R3, L1, L1, R5, L4, L4, L2, R5, R1, R4, L3, L5, L4, R5, L4, R5, R4, L3, L2, L5, R4, R3, L3, R1, L5, R5, R1, L3, R2, L5, R5, L3, R1, R4, L5, R4, R2, R3, L4, L5, R3, R4, L5, L5, R4, L4, L4, R1, R5, R3, L1, L4, L3, L4, R1, L5, L1,R2, R2, R4, R4, L5, R4, R1, L1, L1, L3, L5, L2, R4, L3, L5, L4, L1, R3"
+	moves := strings.Split(move, ", ")
+	curr_x := 0
+	curr_y := 0
+
+	prev := "N"
+	for _, curr := range moves {
+		//fmt.Printf("%d", i)
+		runes := []rune(curr)
+		dir := string(runes[0:1])
+		s1 := string(runes[1 : len(curr)-1])
+		num, _ := strconv.Atoi(s1)
+		switch prev {
+		case "N":
+			switch dir {
+			case "R":
+				curr_x = curr_x + num
+				prev = "E"
+			case "L":
+				curr_x = curr_x - num
+				prev = "W"
+			}
+		case "E":
+			switch dir {
+			case "R":
+				curr_y = curr_y - num
+				prev = "S"
+			case "L":
+				curr_y = curr_y + num
+				prev = "N"
+			}
+		case "S":
+			switch dir {
+			case "R":
+				curr_x = curr_x - num
+				prev = "W"
+			case "L":
+				curr_x = curr_x + num
+				prev = "E"
+			}
+		case "W":
+			switch dir {
+			case "R":
+				curr_y = curr_y + num
+				prev = "N"
+			case "L":
+				curr_y = curr_y - num
+				prev = "S"
+			}
+		}
+	}
+
+	fmt.Printf("%d , %d", curr_x, curr_y)
+}
